18c45d1819fb7b01e4ca0a624ef93139c71cce4f
faithebear <149540831+faithebear@users.noreply.github.com>
2025-04-09 11:07:50 -0600

    aggregated my orders by howmuch they cost

--- models/marts/order_items.sql
+++ models/marts/order_items.sql
@@ -1,66 +1,76 @@
-with
-
-order_items as (
-
-    select * from {{ ref('stg_order_items') }}
-
-),
-
-
-orders as (
-
-    select * from {{ ref('stg_orders') }}
-
-),
-
-products as (
-
-    select * from {{ ref('stg_products') }}
-
-),
-
-supplies as (
-
-    select * from {{ ref('stg_supplies') }}
-
-),
-
-order_supplies_summary as (
-
-    select
-        product_id,
-
-        sum(supply_cost) as supply_cost
-
-    from supplies
-
-    group by 1
-
-),
-
-joined as (
-
-    select
-        order_items.*,
-
-        orders.ordered_at,
-
-        products.product_name,
-        products.product_price,
-        products.is_food_item,
-        products.is_drink_item,
-
-        order_supplies_summary.supply_cost
-
-    from order_items
-
-    left join orders on order_items.order_id = orders.order_id
-
-    left join products on order_items.product_id = products.product_id
-
-    left join order_supplies_summary
-        on order_items.product_id = order_supplies_summary.product_id
-
+WITH stg_order_items AS (
+  SELECT
+    *
+  FROM {{ ref('stg_order_items') }}
+), stg_orders AS (
+  SELECT
+    *
+  FROM {{ ref('stg_orders') }}
+), stg_products AS (
+  SELECT
+    *
+  FROM {{ ref('stg_products') }}
+), stg_supplies AS (
+  SELECT
+    PRODUCT_ID,
+    SUPPLY_COST
+  FROM {{ ref('stg_supplies') }}
+), join_1 AS (
+  SELECT
+    stg_order_items.ORDER_ITEM_ID,
+    stg_order_items.ORDER_ID,
+    stg_order_items.PRODUCT_ID,
+    stg_orders.ORDERED_AT,
+    stg_order_items.PRODUCT_ID AS PRODUCT_ID_1
+  FROM stg_order_items
+  LEFT JOIN stg_orders
+    ON stg_order_items.ORDER_ID = stg_orders.ORDER_ID
+), aggregate_1 AS (
+  SELECT
+    PRODUCT_ID,
+    SUM(SUPPLY_COST) AS SUPPLY_COST
+  FROM stg_supplies
+  GROUP BY
+    PRODUCT_ID
+), join_2 AS (
+  SELECT
+    join_1.ORDER_ITEM_ID,
+    join_1.ORDER_ID,
+    join_1.PRODUCT_ID,
+    join_1.ORDERED_AT,
+    join_1.ORDERED_AT AS ORDERED_AT_1,
+    stg_products.PRODUCT_NAME,
+    stg_products.PRODUCT_PRICE,
+    stg_products.IS_FOOD_ITEM,
+    stg_products.IS_DRINK_ITEM,
+    join_1.PRODUCT_ID AS PRODUCT_ID_1
+  FROM join_1
+  LEFT JOIN stg_products
+    ON join_1.PRODUCT_ID = stg_products.PRODUCT_ID
+), join_3 AS (
+  SELECT
+    join_2.ORDER_ITEM_ID,
+    join_2.ORDER_ID,
+    join_2.PRODUCT_ID,
+    join_2.ORDERED_AT,
+    join_2.PRODUCT_NAME,
+    join_2.PRODUCT_PRICE,
+    join_2.IS_FOOD_ITEM,
+    join_2.IS_DRINK_ITEM,
+    join_2.ORDERED_AT AS ORDERED_AT_1,
+    join_2.PRODUCT_NAME AS PRODUCT_NAME_1,
+    join_2.PRODUCT_PRICE AS PRODUCT_PRICE_1,
+    join_2.IS_FOOD_ITEM AS IS_FOOD_ITEM_1,
+    join_2.IS_DRINK_ITEM AS IS_DRINK_ITEM_1,
+    aggregate_1.SUPPLY_COST
+  FROM join_2
+  LEFT JOIN aggregate_1
+    ON join_2.PRODUCT_ID = aggregate_1.PRODUCT_ID
+), order_items AS (
+  SELECT
+    *
+  FROM join_3
 )
-
-select * from joined
+SELECT
+  *
+FROM order_items
